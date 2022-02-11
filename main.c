7feccf6295104ad8a15ca46e161c914c1898f980
Kokorin Sergey <rezcov442@gmail.com>
2022-02-11 19:23:49 +0300

    - Создан основной файл main.c, в котором реализован ввод фигуры с клавиатуры

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#define _STRING_LENGTH 100
+
+typedef struct
+{
+    float x;
+    float y;
+    float radius;
+} Circle;
+
+int main()
+{
+    Circle circle;
+    char Figure[_STRING_LENGTH];
+
+    printf("Введите фигуру: ");
+    fgets(Figure, _STRING_LENGTH, stdin);
+
+    printf("%s", Figure);
+}
