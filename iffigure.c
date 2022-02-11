6746ba5cd1513ef4347fb9e9d6ad98b873c84b77
Kokorin Sergey <rezcov442@gmail.com>
2022-02-11 21:19:33 +0300

    - Добавлена функция преобразования координат из строки в числа - Добавлена проверка на корректность получившихся координат - Добавлен вывод информации о введенной фигуре

--- iffigure.c
+++ iffigure.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #define __TRUE         1
 #define __FALSE        0
 #define _STRING_LENGTH 100
+#define _INVALID_VALUE -919949
 
 typedef struct
 {
@@ -34,4 +36,79 @@ int _if_staples(char *figure)
   }
 
   return __TRUE;
+}
+
+Circle _read_coordinates(char *figure)
+{
+  Circle coordinates = {
+      .x = _INVALID_VALUE, .y = _INVALID_VALUE, .radius = _INVALID_VALUE};
+  char coordinateX[_STRING_LENGTH];
+  char coordinateY[_STRING_LENGTH];
+  char radius[_STRING_LENGTH];
+  int i = 0;
+
+  while (figure[i] < '0' || figure[i] > '9')
+  {
+    if (i == strlen(figure))
+      return coordinates;
+
+    i++;
+  }
+
+  int j = 0;
+
+  while (figure[i] != ' ')
+  {
+    if (i == strlen(figure))
+      return coordinates;
+
+    coordinateX[j] = figure[i];
+    j++;
+    i++;
+  }
+
+  j = 0;
+
+  while (figure[i] != ',')
+  {
+    if (i == strlen(figure))
+      return coordinates;
+
+    coordinateY[j] = figure[i];
+    j++;
+    i++;
+  }
+
+  j = 0;
+
+  while (figure[i] < '0' || figure[i] > '9')
+  {
+    if (i == strlen(figure))
+      return coordinates;
+
+    i++;
+  }
+
+  while (figure[i] != ')')
+  {
+    if (i == strlen(figure))
+      return coordinates;
+
+    radius[j] = figure[i];
+    j++;
+    i++;
+  }
+
+  coordinates.x = atof(coordinateX);
+  coordinates.y = atof(coordinateY);
+  coordinates.radius = atof(radius);
+
+  return coordinates;
+}
+
+int _if_coordinates(Circle coordinates)
+{
+  if (coordinates.x == _INVALID_VALUE || coordinates.y == _INVALID_VALUE ||
+      coordinates.radius == _INVALID_VALUE)
+    return 0;
 }
--- main.c
+++ main.c
@@ -1,20 +1,59 @@
-#include <stdio.h>
-#include <string.h>
-#define _STRING_LENGTH 100
-
-typedef struct
-{
-  float x;
-  float y;
-  float radius;
-} Circle;
+#include "iffigure.c"
 
 int main()
 {
   Circle circle;
   char Figure[_STRING_LENGTH];
+  int flagCircle = 0;
+  int flagStaples = 0;
+  int flagCoordinates = 0;
+  int i;
 
   printf("Введите фигуру: ");
   fgets(Figure, _STRING_LENGTH, stdin);
-  printf("%s", Figure);
+
+  i = 0;
+
+  flagStaples = _if_staples(Figure);
+  flagCircle = _if_circle(Figure);
+  circle = _read_coordinates(Figure);
+  flagCoordinates = _if_coordinates(circle);
+
+  if (flagCircle)
+  {
+    printf("Название фигуры: ");
+
+    for (i = 0; Figure[i] != '('; i++)
+    {
+      printf("%c", Figure[i]);
+    }
+
+    printf("\n");
+  }
+
+  else
+  {
+    printf("Название фигуры: INCORRECT\n");
+  }
+
+  if (!flagStaples)
+  {
+    printf("Скобки введены не по образцу.\n");
+  }
+
+  if (flagCoordinates)
+  {
+    printf("Координата X: %.4f\n", circle.x);
+    printf("Координата Y: %.4f\n", circle.y);
+    printf("Радиус: %.4f\n", circle.radius);
+  }
+
+  else
+  {
+    printf("Координата X: INCORRECT\n");
+    printf("Координата Y: INCORRECT\n");
+    printf("Радиус: INCORRECT\n");
+  }
+
+  return 0;
 }
